bbe2923b08a020ec03c55c3810629e452264c526
William Doody <wdoody@yahoo.com>
2018-03-26 14:19:15 -0600

    added finish line and new background

--- app.js
+++ app.js
@@ -11,16 +11,23 @@ let move = (e) => {
   if(e.keyCode == 39) {
     carOne +=10;
     car1.style.left = carOne + 'px';
-    if(carOne >= 1000) {
+    if(carOne >= 1050) {
       alert("Car Two Wins!!!");
     }}
 
   if(e.keyCode == 68) {
     carTwo +=10;
     car2.style.left = carTwo + 'px';
-    if(carTwo >= 1000) {
+    if(carTwo >= 1050) {
       alert("Car One Wins!!!");
 }}}
 
 document.onkeydown = move;
+
+
+
+// document.addeventListener('keydown', (event )
+//   if event.keycode === 39) {
+//   console.log
+// }
 

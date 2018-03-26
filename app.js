2c9a91bce8062200d04c5c067aad05d6998d4290
William Doody <wdoody@yahoo.com>
2018-03-26 14:03:13 -0600

    fixed everything

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
-let car1 = document.getElementById("first");
-let car2 = document.getElementById("second");
+let car1 = document.getElementById("carOne");
+let car2 = document.getElementById("carTwo");
 
 
 
@@ -11,14 +11,14 @@ let move = (e) => {
   if(e.keyCode == 39) {
     carOne +=10;
     car1.style.left = carOne + 'px';
-    if(carOne >= 1300) {
+    if(carOne >= 1000) {
       alert("Car Two Wins!!!");
     }}
 
   if(e.keyCode == 68) {
     carTwo +=10;
     car2.style.left = carTwo + 'px';
-    if(carTwo >= 1300) {
+    if(carTwo >= 1000) {
       alert("Car One Wins!!!");
 }}}
 
